e7d180d46e08f8dfdd3ca648addcfa7f010790da
famim2002 <famimhayatyash@gmail.com>
2025-05-25 23:41:55 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="px-2 py-24">
+      <div className="container">
+        <h1 className="mx-auto text-center">todo list</h1>
+              <div className="content-box-css">
+                  <input type="text" placeholder='your text' className='input-css'/>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home
