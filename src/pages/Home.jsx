74b0dce3eaf43ce2bc47fbaa7da93526d77f1e88
famim2002 <famimhayatyash@gmail.com>
2025-05-25 23:52:18 +0600

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <section className="px-2 py-24">
-      <div className="container">
-        <h1 className="mx-auto text-center">todo list</h1>
-              <div className="content-box-css">
-                  <input type="text" placeholder='your text' className='input-css'/>
+      <div className="container flex flex-col gap-20">
+        <h1 className=" header-css">todo list</h1>
+        <div className="content-box-css">
+          <input type="text" placeholder="your text" className="input-css" />
+          <div className="flex gap-5">
+            <button className="input-btn-css">submit</button>
+            {/* <button className="input-btn-css"></button> */}
+          </div>
         </div>
       </div>
     </section>
